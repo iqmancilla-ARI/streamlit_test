4697fa11744c3e1650c19ff5c9ab73d9545cd3f0
iqmancilla-ARI <iq.mancilla@gmail.com>
2026-03-23 18:12:09 -0600

    El texto a imprimir le faltaba una n en la palabra construcción

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.header('Lanzar una moneda')
 
-st.write('Esta aplicación aún no es funcional. En costrucción')
+st.write('Esta aplicación aún no es funcional. En construcción')
 
